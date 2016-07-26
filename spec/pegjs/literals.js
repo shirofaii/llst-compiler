4ac0858c562dc3469f9af95558c59012dd9244b2
Scorik Egor <shirofaii@gmail.com>
2016-07-26 20:15:54 +0300

    string parsing tests

--- spec/pegjs/literals.js
+++ spec/pegjs/literals.js
@@ -1,4 +1,5 @@
-var parser = require('../../build/llst-method.js')
+const parser = require('../../build/llst-method.js')
+const SyntaxError = parser.SyntaxError
 
 function parse(rule, text) {
     return parser.parse(text, {startRule: rule})
@@ -6,6 +7,11 @@ function parse(rule, text) {
 
 describe('llst literals grammar', function() {
     var parseNumber = text => parse('number', text)
+    var tryParseNumber = function(text) { return () => {parseNumber(text)} }
+    
+    var parseString = text => parse('string', text)
+    var tryParseString = function(text) { return () => {parseString(text)} }
+    
     it('integers', function() {
         expect(parseNumber('123')).node('number')
         expect(parseNumber('123')).nodeWithValue(123)
@@ -16,6 +22,12 @@ describe('llst literals grammar', function() {
         expect(parseNumber('-0')).nodeWithValue(0)
         expect(parseNumber('02')).nodeWithValue(2)
         expect(parseNumber('1267650600228229401496703205376')).nodeWithValue(Math.pow(2, 100))
+        
+        expect(tryParseNumber('+-1')).toThrowError(SyntaxError)
+        expect(tryParseNumber('1+')).toThrowError(SyntaxError)
+        expect(tryParseNumber('1a')).toThrowError(SyntaxError)
+        expect(tryParseNumber('')).toThrowError(SyntaxError)
+        expect(tryParseNumber('-')).toThrowError(SyntaxError)
     });
     
     it('floats', function() {
@@ -27,7 +39,32 @@ describe('llst literals grammar', function() {
         expect(parseNumber('+0.0')).nodeWithValue(0)
         expect(parseNumber('-0.0')).nodeWithValue(0)
         expect(parseNumber('02.20')).nodeWithValue(2.2)
-        expect(parseNumber('0.0101')).nodeWithValue(0.0101)
+        expect(parseNumber('0.01010')).nodeWithValue(0.01010)
+        
+        expect(tryParseNumber('1.')).toThrowError(SyntaxError)
+        expect(tryParseNumber('.1')).toThrowError(SyntaxError)
+        expect(tryParseNumber('.')).toThrowError(SyntaxError)
     });
 
+    it('string', function() {
+        expect(parseString("'123'")).node('string')
+        expect(parseString("'123'")).nodeWithValue('123')
+        expect(parseString("''")).nodeWithValue('')
+        expect(parseString("' '")).nodeWithValue(' ')
+        expect(parseString("'abc'")).nodeWithValue('abc')
+        expect(parseString("' 1aBC'")).nodeWithValue(' 1aBC')
+        expect(parseString("'Testing «ταБЬℓσ»: 1<2 & 4+1>3, now 20% off!'")).nodeWithValue('Testing «ταБЬℓσ»: 1<2 & 4+1>3, now 20% off!')
+        expect(parseString("''''")).nodeWithValue("\'")
+        expect(parseString("'\"'")).nodeWithValue("\"")
+        
+        expect(tryParseNumber("abc")).toThrowError(SyntaxError)
+        expect(tryParseNumber("\"abc\"")).toThrowError(SyntaxError)
+        expect(tryParseNumber("'abc")).toThrowError(SyntaxError)
+        expect(tryParseNumber("a'bc")).toThrowError(SyntaxError)
+        expect(tryParseNumber("a'bc'")).toThrowError(SyntaxError)
+        expect(tryParseNumber("abc'")).toThrowError(SyntaxError)
+        expect(tryParseNumber("'''")).toThrowError(SyntaxError)
+    })
+    
+    
 });
